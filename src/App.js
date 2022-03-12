43b8168e2565bc28df26ad91dfd9db329e987ea9
Amaan85277 <91450039+Amaan85277@users.noreply.github.com>
2022-03-12 17:25:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,26 +17,49 @@ const App = () => {
   const [isCross, setIsCross] = useState(false);
   const [winMessage, setWinMessage] = useState("");
 
-  const reloadgame = () => {
+  const reloadGame = () => {
     setIsCross(false);
     setWinMessage("");
     itemArray.fill("empty", 0, 9);
   }
 
   const checkIsWinner = () => {
-    //
+    if(itemArray[0]!=="empty" && itemArray[0]===itemArray[1] && itemArray[0]===itemArray[2]){
+      setWinMessage(`${itemArray[0]} won`);
+    }
+    else if(itemArray[3]!=="empty" && itemArray[3]===itemArray[3] && itemArray[3]===itemArray[5]){
+      setWinMessage(`${ itemArray[3]} won`)
+    }
+    else if(itemArray[6]!=="empty" && itemArray[6]===itemArray[7] && itemArray[6]===itemArray[8]){
+      setWinMessage(`${ itemArray[6]} won`)
+    }
+    else if(itemArray[0]!=="empty" && itemArray[0]===itemArray[3] && itemArray[0]===itemArray[6]){
+      setWinMessage(`${ itemArray[0]} won`)
+    }
+    else if(itemArray[1]!=="empty" && itemArray[1]===itemArray[4] && itemArray[1]===itemArray[7]){
+      setWinMessage(`${ itemArray[1]} won`)
+    }
+    else if(itemArray[2]!=="empty" && itemArray[2]===itemArray[5] && itemArray[2]===itemArray[8]){
+      setWinMessage(`${ itemArray[2]} won`)
+    }
+    else if(itemArray[0]!=="empty" && itemArray[0]===itemArray[4] && itemArray[0]===itemArray[8]){
+      setWinMessage(`${ itemArray[0]} won`)
+    }
+    else if(itemArray[2]!=="empty" && itemArray[2]===itemArray[4] && itemArray[2]===itemArray[6]){
+      setWinMessage(`${ itemArray[2]} won`)
+    }
   }
 
   const changeItem = itemNumber => {
-     if(winMessage){
-       return toast(winMessage, {type:"success"});
-     }
+    if (winMessage) {
+      return toast(winMessage, { type: "success" });
+    }
 
-    if(itemArray[itemNumber] === "empty"){
+    if (itemArray[itemNumber] === "empty") {
       itemArray[itemNumber] = isCross ? "cross" : "circle";
       setIsCross(!isCross); // just flipping the switch here
-    }else{
-      return toast("already filled", {type:"error"});
+    } else {
+      return toast("already filled", { type: "error" });
     }
 
     // after every move/iteration we need to check if there' any winner
@@ -48,9 +71,24 @@ const App = () => {
       <ToastContainer position="bottom-center" />
       <Row>
         <Col md={6} className="offset-md-3">
+          {winMessage ? (
+            <div className="mb-2 mt2">
+              <h1 className="text-success text-uppercase text-center">
+                {winMessage}
+              </h1>
+              <Button color='success' block onClick={reloadGame}>
+                Reload the game
+              </Button>
+            </div>
+          ) : (
+            <h1 className="text-center text-warning">
+              {isCross ? "Cross's" : "Circle's"} turn
+            </h1>
+          )}
+
           <div className="grid">
             {itemArray.map((item, index) => (
-              <Card>
+              <Card  color="warning" onClick={ ()=> changeItem(index) }>
                 <CardBody className="box">
                   <Icon name={item} />
                 </CardBody>
